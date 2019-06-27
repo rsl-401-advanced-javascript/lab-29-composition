2f8558acefc5feab511ff90b487e9014d49acd73
RoryLetteney <rory.letteney@gmail.com>
2019-06-27 15:17:37 -0500

    Phase 2 complete

---
+++ src/functional-story/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import FunctionalDetails from '../functional-details';
+
+export default () => {
+  return (
+    <FunctionalDetails>
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+    </FunctionalDetails>
+  );
+}
